60204b9389fb227bd401c624e40ce9ca239659fa
Chanaka Lakmal <ldclakmal@gmail.com>
2019-04-20 17:08:12 +0530

    Update go passthrough with parameterized program

--- src/http/passthrough.go
+++ src/http/passthrough.go
@@ -1,12 +1,20 @@
 // How to run the program
 //
 // Run as HTTP/1.1 passthrough
-// $ go run passthrough.go
-// Test - $ curl -k https://localhost:9090/passthrough -d "Hello"
+// $ go run passthrough.go -host localhost -port 9191 -path /hello/sayHello -certfile ../cert/server.crt -keyfile ../cert/server.key
+//
+// Test with HTTP/1.1 POST request
+// $ curl -k https://localhost:9090/passthrough -d "Hello Go!"
+//
 //
 // Run as HTTP/2 passthrough
-// $ go run passthrough.go -version 2 -maxstream 100
-// Test - $ curl --http2 -k https://localhost:9090/passthrough -d "Hello"
+// $ go run passthrough.go -version 2 -maxstream 100 -host localhost -port 9191 -path /hello/sayHello -certfile ../cert/server.crt -keyfile ../cert/server.key
+//
+// Test with HTTP/1.1 POST request
+// $ curl -k https://localhost:9090/passthrough -d "Hello Go!"
+//
+// Test with HTTP/2 POST request
+// $ curl --http2 -k https://localhost:9090/passthrough -d "Hello Go!"
 //
 // NOTE: The relevant backend should be up and running in order to test.
 
@@ -25,6 +33,13 @@ import (
 // By default version flag is set to 1 (refers to HTTP/1.1)
 var httpVersion = flag.Int("version", 1, "HTTP version")
 
+var backendhost = flag.String("host", "localhost", "Server host")
+var backendport = flag.String("port", "9191", "Server port")
+var backendpath = flag.String("path", "/hello/sayHello", "Server Request path")
+
+var certFile = flag.String("certfile", "", "SSL certificate file")
+var keyFile = flag.String("keyfile", "", "SSL certificate key file")
+
 // By default the number of maximum concurrent streams per connection is set as 1000
 var maxConcurrentStreams = flag.Int("maxstream", 1000, "HTTP/2 max concurrent streams")
 
@@ -35,7 +50,7 @@ func main() {
 
 	// Create a pool with the server certificate since it is not signed
 	// by a known CA
-	caCert, err := ioutil.ReadFile("../cert/server.crt")
+	caCert, err := ioutil.ReadFile(*certFile)
 	if err != nil {
 		log.Fatalf("Reading server certificate: %s", err)
 	}
@@ -58,7 +73,7 @@ func main() {
 		}
 		http.HandleFunc("/passthrough", reverseProxy)
 		log.Printf("Go Pssthrough: { HTTPVersion = 1 }; serving on https://localhost:9090/passthrough")
-		log.Fatal(httpServer.ListenAndServeTLS("../cert/server.crt", "../cert/server.key"))
+		log.Fatal(httpServer.ListenAndServeTLS(*certFile, *keyFile))
 	case 2:
 		client.Transport = &http2.Transport{
 			TLSClientConfig: tlsConfig,
@@ -72,13 +87,13 @@ func main() {
 		_ = http2.ConfigureServer(&httpServer, &http2Server)
 		http.HandleFunc("/passthrough", reverseProxy)
 		log.Printf("Go Pssthrough: { HTTPVersion = 2, MaxStreams = %v }; serving on https://localhost:9090/passthrough", *maxConcurrentStreams)
-		log.Fatal(httpServer.ListenAndServeTLS("../cert/server.crt", "../cert/server.key"))
+		log.Fatal(httpServer.ListenAndServeTLS(*certFile, *keyFile))
 	}
 }
 
 func reverseProxy(w http.ResponseWriter, req *http.Request) {
 	log.Printf("Request connection: %s, path: %s", req.Proto, req.URL.Path[1:])
-	backendReq, _ := http.NewRequest(req.Method, "https://localhost:9191/hello/sayHello", req.Body)
+	backendReq, _ := http.NewRequest(req.Method, "https://"+*backendhost+":"+*backendport+*backendpath, req.Body)
 	resp, _ := client.Do(backendReq)
 	body, _ := ioutil.ReadAll(resp.Body)
 	_, _ = w.Write(body)
